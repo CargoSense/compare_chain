1c51f029ae1756c2983280b0aa97d784c392149a
William Lanchantin <william.lanchantin@cargosense.com>
2022-11-01 17:32:08 -0400

    make with with general node

--- lib/compare_chain.ex
+++ lib/compare_chain.ex
@@ -3,20 +3,18 @@ defmodule CompareChain do
   defmacro compare?(expr, module) do
     ast = quote(do: unquote(expr))
 
-    %{pairs: pairs} =
-      ast
-      |> Macro.postwalker()
-      |> Enum.reduce(%{pairs: [], prev: []}, fn
-        {op, _, _}, %{pairs: pairs, prev: [b, a]} = acc when op in @valid_ops ->
-          %{acc | pairs: [{op, a, b} | pairs], prev: [b]}
+    ast
+    |> Macro.prewalker()
+    |> Enum.reduce_while([], fn
+      {op, _, [_left, right]}, acc when op in @valid_ops ->
+        {:cont, [{op, right} | acc]}
 
-        {_, _, nil} = var, %{prev: prev} = acc ->
-          %{acc | prev: [var | prev]}
-      end)
-
-    pairs
-    |> Enum.map(fn {op, a, b} ->
-      {compare, result} =
+      node, acc ->
+        {:halt, [{nil, node} | acc]}
+    end)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.reduce(true, fn [{_, left}, {op, right}], acc ->
+      {kernel_fun, evals_to} =
         case op do
           :< -> {:==, :lt}
           :> -> {:==, :gt}
@@ -24,13 +22,13 @@ defmodule CompareChain do
           :>= -> {:!=, :lt}
         end
 
+      comparison =
+        quote do
+          unquote(module).compare(unquote(left), unquote(right))
+        end
+
       quote do
-        Kernel.unquote(compare)(unquote(module).compare(unquote(a), unquote(b)), unquote(result))
-      end
-    end)
-    |> Enum.reduce(true, fn expr, acc ->
-      quote do
-        unquote(expr) and unquote(acc)
+        unquote(acc) and Kernel.unquote(kernel_fun)(unquote(comparison), unquote(evals_to))
       end
     end)
   end
@@ -39,11 +37,14 @@ defmodule CompareChain do
     a = ~U[2020-01-01 00:00:00Z]
     b = ~U[2021-01-01 00:00:00Z]
     c = ~U[2022-01-01 00:00:00Z]
+    d = ~U[2023-01-01 00:00:00Z]
 
-    # true
+    # # true
     compare?(a < b <= c, DateTime) |> IO.inspect()
 
     # false
-    compare?(a >= b, DateTime) |> IO.inspect()
+    compare?(%{b: b}[:b] >= d, DateTime) |> IO.inspect()
+
+    :ok
   end
 end
