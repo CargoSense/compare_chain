76b3a317ada16e0c4a8942a609aa7ef323b9c7da
William Lanchantin <william.lanchantin@cargosense.com>
2022-11-04 16:00:48 -0400

    use simpler compile time test approach

--- test/compile_time_test.exs
+++ test/compile_time_test.exs
@@ -1,20 +1,30 @@
 defmodule CompileTimeTest do
+  # This module tests some compile-time behavior.
+  #
+  # The code in each test's `fun/0` won't compile, but that fact is hidden when
+  # it's wrapped in a `defmodule`.
+  #
+  # Based on examples found in:
+  # https://github.com/phoenixframework/phoenix/blob/master/test/phoenix/verified_routes_test.exs
   use ExUnit.Case
 
   import CompareChain.ErrorMessage
-  import CompileTimeAssertions
 
   test "including no comparison operators raises at compile time" do
-    assert_compile_time_raise(ArgumentError, chain_error_message(), fn ->
-      import CompareChain
-      compare?(5)
+    assert_raise(ArgumentError, chain_error_message(), fn ->
+      defmodule NoOperators do
+        import CompareChain
+        def fun, do: compare?(5)
+      end
     end)
   end
 
   test "including not in message raises at compile time" do
-    assert_compile_time_raise(ArgumentError, raise_on_not_message(), fn ->
-      import CompareChain
-      compare?(not (1 > 2))
+    assert_raise(ArgumentError, raise_on_not_message(), fn ->
+      defmodule Not do
+        import CompareChain
+        def fun, do: compare?(not (1 > 2))
+      end
     end)
   end
 end
