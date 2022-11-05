695b75cea05fda6db358c26101355b5383e9ad74
William Lanchantin <william.lanchantin@cargosense.com>
2022-11-05 11:38:22 -0400

    bump to v0.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CompareChain.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/CargoSense/compare_chain"
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
